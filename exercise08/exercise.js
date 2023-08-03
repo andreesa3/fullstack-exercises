2bb4dabed0efb8afb59fda60587112d9b80568aa
Andrea <andrea.saitta.as@gmail.com>
2023-08-03 12:06:48 +0200

    esercizio 8 js

---
+++ exercise08/exercise.js
@@ -0,0 +1,11 @@
+function multiplyByTwo(value) {
+  let number = 2;
+  function inner() {
+    console.log(value * number);
+  }
+  return inner;
+}
+
+multiplyByTwo(10)();
+multiplyByTwo(20)();
+multiplyByTwo(3)();
