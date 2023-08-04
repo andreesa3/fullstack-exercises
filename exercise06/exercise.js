df9788704000b29774e5cf43e64ff2be443e3f07
Andrea <andrea.saitta.as@gmail.com>
2023-08-04 10:49:59 +0200

    esercizio 6 js

--- exercise06/exercise.js
+++ exercise06/exercise.js
@@ -1,9 +1,10 @@
 function printName() {
   let helloName = "Hello John";
   function inner() {
-    console.log(helloName);
+    return helloName;
   }
   return inner;
 }
 
 const result = printName()();
+console.log(result);
