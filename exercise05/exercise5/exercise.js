184564ee3716071c4391d4c2906663eb43e063a0
Andrea <andrea.saitta.as@gmail.com>
2023-08-24 17:52:48 +0200

    esercizio 5 js

--- exercise05/exercise5/exercise.js
+++ exercise05/exercise5/exercise.js
@@ -9,5 +9,10 @@ const person2 = Object.assign({}, person1);
 // Modify the property `firstName` of the `person2` in "Simon".
 person2.firstName = "Simon";
 
+// Se non usassimo Object.assign o lo spread operator ci riferiremmo 
+// allo stesso oggetto in memoria mentre con questi due metodi stiamo 
+// creando due oggetti in memoria che hanno la stessa struttura ma sono 
+// indipendenti
+
 console.log(person1);
 console.log(person2);
