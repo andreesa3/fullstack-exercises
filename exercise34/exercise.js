130e2f42aa95f0eb265c5bd226a0375bb02c2f0a
Andrea <andrea.saitta.as@gmail.com>
2023-09-04 18:41:50 +0200

    esercizio 34 js

--- exercise34/exercise.js
+++ exercise34/exercise.js
@@ -1,10 +1,12 @@
 function printAsyncName(callback, name) {
   setTimeout(callback, 1000);
 
+  /* Metto 3000 in modo che parta dopo 2 secondi dalla prima pausa */
   let interval = setInterval(() => {
     console.log(name);
   }, 3000);
 
+  /* Interrompo qui perché volevo provare anche se non richiesto dalla consegna */
   setTimeout(() => {
     clearInterval(interval);
   }, 3500)
