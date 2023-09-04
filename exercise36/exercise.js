0bc8cda87491570d6e5d71e7bb509357e776a52e
Andrea <andrea.saitta.as@gmail.com>
2023-09-04 18:42:26 +0200

    esercizio 36 js

---
+++ exercise36/exercise.js
@@ -0,0 +1,10 @@
+function repeatHello(callback) {
+  const interval = setInterval(callback, 1000);
+  setTimeout(() => {
+    clearInterval(interval);
+  }, 5000)
+}
+
+const sayHello = () => console.log('Hello');
+
+repeatHello(sayHello); 
