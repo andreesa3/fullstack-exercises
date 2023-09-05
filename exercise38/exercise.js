e60efc8e48f9cc7dad7ed85b448d73d701320761
Andrea <andrea.saitta.as@gmail.com>
2023-09-05 11:09:25 +0200

    esercizio 38 js

---
+++ exercise38/exercise.js
@@ -0,0 +1,24 @@
+const isLogged = true;
+
+const isUserLogged = new Promise((resolve, reject) => {
+  if (isLogged) {
+    resolve(Math.random());
+  } else {
+    reject(new Error('You are not logged in!'));
+  }
+})
+
+const getUserData = (rand) => {
+  return new Promise((resolve, reject) => {
+    if (rand > 0.5) {
+      resolve({ name: "John", age: 24 });
+    } else {
+      reject(new Error('Num is smaller than 0.5'))
+    }
+  })
+}
+
+isUserLogged
+  .then((rand) => getUserData(rand))
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err))
