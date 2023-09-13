b470e978bb6607a47dcd794e6a464c70a31a2ca2
Andrea <andrea.saitta.as@gmail.com>
2023-09-13 17:33:12 +0200

    esercizio 44 js

---
+++ exercise44/exercise.js
@@ -0,0 +1,16 @@
+const user = {
+  id: 1,
+  name: "John",
+  age: 25,
+};
+
+const getData = (data) => {
+  try {
+    return JSON.parse(localStorage.getItem(data));
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+console.log(getData('user'));
+
